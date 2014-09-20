8a613d6a4cebee7ef7daac1795dc40518825aa7a
Dan Kempster <dev@dankempster.co.uk>
2014-09-20 20:54:10 +0100

    Add mocked iterator test for PHPUnit

---
+++ src/Axstrad/Component/Test/Example/ExampleList.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of the Axstrad library.
+ *
+ * (c) Dan Kempster <dev@dankempster.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Dan Kempster <dev@dankempster.co.uk>
+ * @package Axstrad\Component\Iterator
+ */
+namespace Axstrad\Component\Test\Example;
+
+
+/**
+ * Axstrad\Component\Test\Example\ExampleList
+ */
+class ExampleList implements
+	\Iterator
+{
+	public function current()
+	{
+
+	}
+
+	public function key()
+	{
+
+	}
+
+	public function next()
+	{
+
+	}
+
+	public function rewind()
+	{
+
+	}
+
+	public function valid()
+	{
+
+	}
+}
