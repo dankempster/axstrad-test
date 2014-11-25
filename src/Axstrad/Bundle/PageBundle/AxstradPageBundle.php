cfb42464f3da4b13de3ebc399de483cc500b9e08
Dan Kempster <dev@dankempster.co.uk>
2014-11-25 19:24:49 +0000

    Start page-bundle

---
+++ src/Axstrad/Bundle/PageBundle/AxstradPageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Axstrad\Bundle\PageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AxstradPageBundle extends Bundle
+{
+}
---
+++ src/Axstrad/Bundle/PageBundle/Entity/Page.php
@@ -0,0 +1,13 @@
+<?php
+namespace Axstrad\Bundle\PageBundle\Entity;
+
+use Axstrad\Component\Content\Entity\Article;
+
+
+/**
+ * Axstrad\Bundle\PageBundle\Entity\Page
+ */
+class Page extends Article
+{
+
+}
