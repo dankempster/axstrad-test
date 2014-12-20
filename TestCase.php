67a864d615eca8d0556b90d6dbbd12b828496adb
Dan Kempster <dev@dankempster.co.uk>
2014-12-20 16:40:54 +0000

    Make Axstrad\Component\Test\TestCase an abstract class

--- TestCase.php
+++ TestCase.php
@@ -5,7 +5,7 @@ namespace Axstrad\Component\Test;
 /**
  * Axstrad\Component\Test\TestCase
  */
-class TestCase extends \PHPUnit_Framework_TestCase
+abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
     protected $fixture = null;
 }
